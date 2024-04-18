8ebe82f221120828261d8d9fd588855fc510d766
Luis Roberto Roque Vargas <luisrqvg@gmail.com>
2024-04-17 19:11:17 -0700

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://Shoul136.github.io/pwa-despliegue/sw.js', { scope: 'https://Shoul136.github.io/pwa-despliegue/' })})}
